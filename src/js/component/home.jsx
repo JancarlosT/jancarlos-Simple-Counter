eddbcf6bf3c1577b4e108c7639c99d278bd32653
JancarlosT <138736732+JancarlosT@users.noreply.github.com>
2024-01-26 02:52:57 +0000

    Simple Counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,21 +4,21 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">Simple Counter</h1>
+			<div className="container box">
+				<div className="row">
+					<div className="col conter"><i className="far fa-clock"></i></div>
+					<div className="col conter">{props.seis}</div>
+					<div className="col conter">{props.cinco}</div>
+					<div className="col conter">{props.cuatro}</div>
+					<div className="col conter">{props.tree}</div>
+					<div className="col conter">{props.dos}</div>
+					<div className="col conter">{props.uno}</div>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,30 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
 // include your styles into the webpack bundle
 import "../styles/index.css";
-
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let secons = 0
+
+setInterval(() => {
+    const one = Math.floor((secons / 1) % 10);
+    const two = Math.floor((secons / 10) % 10);
+    const three = Math.floor((secons / 100) % 10);
+    const four = Math.floor((secons / 1000) % 10);
+    const five = Math.floor((secons / 10000) % 10);
+    const six = Math.floor((secons / 100000) % 10);
+    secons += 1
+
+    //render your react application
+    ReactDOM.render(<Home 
+        uno = {one}
+        dos = {two}
+        tree = {three}
+        cuatro = {four}
+        cinco = {five}
+        seis = {six} 
+    />, document.querySelector("#app"));
+
+},1000)
